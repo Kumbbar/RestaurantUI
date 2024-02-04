e18a88fa87af3264115e5acd59249ea4cf1b0a26
Kumbbar <damirnasibullin8@gmail.com>
2024-02-04 20:52:43 +0500

    0.3 start main page layout

---
+++ core/tiles_list.py
@@ -0,0 +1,34 @@
+from controls.items import NavigationTile
+import flet_core as ft
+
+PASS = ft.Container(
+            height=400,
+            width=300,
+            bgcolor='yellow'
+)
+
+
+def get_admin_tiles():
+    admin_tiles = [
+        NavigationTile(
+            'USERS',
+            ft.icons.PERSON,
+            next_control=PASS
+        ),
+        NavigationTile(
+                'PERMISSIONS',
+                ft.icons.RULE,
+                next_control=PASS
+        ),
+        NavigationTile(
+                    'GROUPS',
+                    ft.icons.GROUP,
+                    next_control=PASS
+        ),
+        NavigationTile(
+                    'CONTENT TYPES',
+                    ft.icons.DATASET,
+                    next_control=PASS
+        )
+    ]
+    return admin_tiles
